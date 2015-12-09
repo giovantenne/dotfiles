dd551b8dd7a0dad6f99abcf4d55b29dfaba6fcf2
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2015-12-09 18:02:37 +0100

    simpler prompt

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -123,18 +123,18 @@ function prompt_command() {
   # http://twitter.com/cowboy/status/150254030654939137
   PS1="\n"
   # svn: [repo:lastchanged]
-  PS1="$PS1$(prompt_svn)"
+  # PS1="$PS1$(prompt_svn)"
   # git: [branch:flags]
-  PS1="$PS1$(prompt_git)"
+  # PS1="$PS1$(prompt_git)"
   # hg:  [branch:flags]
-  PS1="$PS1$(prompt_hg)"
+  # PS1="$PS1$(prompt_hg)"
   # misc: [cmd#:hist#]
   # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
   # path: [user@host:path]
   PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
-  PS1="$PS1\n"
   # date: [HH:MM:SS]
-  PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+  # PS1="$PS1\n"
+  # PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
   # exit code: 127
   PS1="$PS1$(prompt_exitcode "$exit_code")"
   PS1="$PS1 \$ "

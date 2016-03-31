72011d748fd74529cbc3f79052fb9377b9a6de34
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2016-03-31 15:17:54 +0200

    removed whois alias

--- source/50_net.sh
+++ source/50_net.sh
@@ -1,6 +1,6 @@
 # IP addresses
 alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias whois="whois -h whois-servers.net"
+# alias whois="whois -h whois-servers.net"
 
 # Flush Directory Service cache
 alias flush="dscacheutil -flushcache"

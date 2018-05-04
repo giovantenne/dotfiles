c9db0c8d4b2dcae63fffce65cb80ded7c1e6043d
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2018-05-04 10:07:37 +0200

    fix

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ done
 ln -sfv ~/.dotfiles/.dotfiles_linker ~/
 source ~/.dotfiles_linker
 
-grep -q -F ". ~/.dotfiles_linker" ~/.bashrc || echo ". ~/.dotfiles_linker" >> ~.bashrc
+grep -q -F ". ~/.dotfiles_linker" ~/.bashrc || echo ". ~/.dotfiles_linker" >> ~/.bashrc
 
 # Backups, swaps and undos are stored here.
 mkdir -p ~/.dotfiles/caches/vim

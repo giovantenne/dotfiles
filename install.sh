c95c23baed3a33ee2624bbace0cd485ce9bf7e74
Claudio Benvenuti <claudio@benve.it>
2024-02-02 17:11:55 +0100

    fix

--- install.sh
+++ install.sh
@@ -34,6 +34,8 @@ fi
 
 # ASDF
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
 source ~/.bashrc
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
